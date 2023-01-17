ebb9a64d5209917687d3e4480a664cb0fa028c21
alejandro <alee_2695@live.com.ar>
2023-01-17 11:07:19 -0300

    Se actualiza firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDo5Cfv2o4KiRC91y_bGb8iSherhxPDTnI",
+  authDomain: "drivenvision-ddf94.firebaseapp.com",
+  projectId: "drivenvision-ddf94",
+  storageBucket: "drivenvision-ddf94.appspot.com",
+  messagingSenderId: "351186324919",
+  appId: "1:351186324919:web:1590b05425c3e3186d2c32"
+  }
 };
